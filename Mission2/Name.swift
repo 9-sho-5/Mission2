d906ca9b6de3a655b308a0c7e3db9c2d304490c8
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-24 10:51:56 +0900

    missoin2

---
+++ Mission2/Name.swift
@@ -0,0 +1,21 @@
+//
+//  Name.swift
+//  Mission2
+//
+//  Created by Kusunose Hosho on 2020/05/24.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import Foundation
+
+class Name {
+    var name: String!
+    
+    var number: String!
+    
+    init(number: String, name: String) {
+        
+        self.number = number
+        self.name = name
+    }
+}
