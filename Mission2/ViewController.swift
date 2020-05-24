b5cdbb77e963c375700a3162c8745326694c0860
Hosho Kusunose <hosho.kusu@icloud.com>
2020-05-24 10:39:47 +0900

    Initial Commit

---
+++ Mission2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mission2
+//
+//  Created by Kusunose Hosho on 2020/05/24.
+//  Copyright © 2020 Kusunose Hosho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
